efef331779e9e397d5328b5eac9ffdf014c2fbc1
yanyan <1716330643@qq.com>
2017-11-17 13:54:22 +0800

    first commit

---
+++ src/main/java/yy/jar/shadow/ShadowApplication.java
@@ -0,0 +1,12 @@
+package yy.jar.shadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowApplication.class, args);
+	}
+}
